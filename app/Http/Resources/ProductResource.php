279c6d3d2b9ac2d58d08ffec36dba9d28efcf647
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-12-25 04:30:13 -0300

    Create product resource

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+
+        return [
+            'uuid' => $this->uuid,
+            'name' => $this->name,
+            'description' => $this->description,
+            'image' => $this->image,
+            'slug' => $this->slug,
+            'date' => Carbon::make($this->created_at)->format('Y-m-d'),
+        ];
+    }
+}
