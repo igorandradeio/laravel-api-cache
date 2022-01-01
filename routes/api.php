6cc4e97245fef4b6377e0c15caec6b79d77dad54
Igor Andrade <oigorwebs@gmail.com>
2022-01-01 01:59:09 -0300

    Authenticated routes

--- routes/api.php
+++ routes/api.php
@@ -8,16 +8,31 @@ use App\Http\Controllers\Auth\AuthApiController;
 use Illuminate\Support\Facades\Route;
 
 
+/** User login */
 Route::post('/auth', [AuthApiController::class, 'authenticate']);
 
-Route::apiResource('/categories/{category}/products', ProductController::class);
-
-Route::put('/categories/{uuid}', [CategoryController::class, 'update']);
-Route::delete('/categories/{uuid}', [CategoryController::class, 'destroy']);
-Route::get('/categories/{uuid}', [CategoryController::class, 'show']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::get('/categories', [CategoryController::class, 'index']);
+//Auth Routes
+Route::middleware(['apiJWT'])->group(function () {
+    /** User authenticated information */
+    Route::get('/me', [AuthApiController::class, 'getAuthenticatedUser']);
+    /** Refresh user Token */
+    Route::post('/auto-refresh', [AuthApiController::class, 'refreshToken']);
+    /** Product routes */
+    Route::post('/logout', [AuthApiController::class, 'logout']);
+    /** Product routes */
+    Route::apiResource('/categories/{category}/products', ProductController::class);
+    /** Update category by uuid*/
+    Route::put('/categories/{uuid}', [CategoryController::class, 'update']);
+    /** Delete category by uuid*/
+    Route::delete('/categories/{uuid}', [CategoryController::class, 'destroy']);
+    /** Get category by uuid*/
+    Route::get('/categories/{uuid}', [CategoryController::class, 'show']);
+    /** Create category */
+    Route::post('/categories', [CategoryController::class, 'store']);
+    /** Get all categories */
+    Route::get('/categories', [CategoryController::class, 'index']);
+});
 
 Route::get('/', function () {
-    return response()->json(['message' => 'ola mundo']);
+    return response()->json(['message' => 'Hello world']);
 });
