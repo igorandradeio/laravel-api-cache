9f47a4c4352ec53da91f6425329531610875e2b2
Igor Andrade <oigorwebs@gmail.com>
2022-01-17 22:24:56 -0300

    Add resource route

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,11 @@
 
 use App\Http\Controllers\Api\{
     CategoryController,
-    ProductController
+    ProductController,
+    ResourceController
 };
 use App\Http\Controllers\Auth\AuthApiController;
 use Illuminate\Support\Facades\Route;
-
 
 /** User login */
 Route::post('/auth', [AuthApiController::class, 'authenticate']);
@@ -31,6 +31,8 @@ Route::middleware(['apiJWT'])->group(function () {
     Route::post('/categories', [CategoryController::class, 'store']);
     /** Get all categories */
     Route::get('/categories', [CategoryController::class, 'index']);
+    /** Get all resources */
+    Route::get('/resources', [ResourceController::class, 'index']);
 });
 
 Route::get('/', function () {
